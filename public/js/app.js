bbfbdd7e63e0ed0535e645e20425243eb47bdb87
Fabio Gelsomini <fgelsomini@gmail.com>
2016-06-06 00:08:44 -0400

    Changes to tabbed filter list

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
+
+function initToggler() {
+  $('#filter-tabs').on('click.toggler', 'li.visible.is-active a', function (event) {
+    event.preventDefault();
+    $('#hidden-close-all-tab a').click();
+    return false;
+  });
+}
+
 $(document).foundation();
+
+$(document).ready(function() {
+  if ($('#filter-tabs').length > 0) {
+    initToggler();  
+  } else {
+    console.log('No filter tabs found.');
+  }
+});
